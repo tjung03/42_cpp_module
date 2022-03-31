5fe22275361002785baad909f4d04284306d48ad
huntae-42 <tjung@student.42seoul.kr>
2022-03-31 19:46:27 +0900

    cpp02: bsp 기능 수정 - 변과 꼭지점은 false 로

--- cpp_module_02/ex03/bsp.cpp
+++ cpp_module_02/ex03/bsp.cpp
@@ -6,11 +6,26 @@
 /*   By: tjung <tjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 05:07:39 by tjung             #+#    #+#             */
-/*   Updated: 2022/03/26 21:33:06 by tjung            ###   ########.fr       */
+/*   Updated: 2022/03/31 19:42:07 by tjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
+
+bool	checkFunc(const Point p1, const Point p2, const Point target, int *cross_cnt)
+{
+	Fixed cross_pnt;
+	
+	if ((target.get_y() < p1.get_y()) != target.get_y() < p2.get_y())
+	{
+		cross_pnt = ((p2.get_x() - p1.get_x()) * (target.get_y() - p1.get_y()) / (p2.get_y() - p1.get_y())) + p1.get_x();
+		if (cross_pnt == target.get_x())
+			return (false);
+		if (target.get_x() < cross_pnt)
+			++(*cross_cnt);
+	}
+	return (true);
+}
 
 //	점 point 의 y 좌표는 두 선분 꼭지점의 y 좌표 사이에 있다.
 //	점 point 를 지나는 수평선과 선분 사이의 교점의 x 좌표는 점 point 의 x 좌표보다 크다.
@@ -20,25 +35,19 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 	int		cross_cnt = 0;
 
 	// a-b
-	if ((point.get_y() < a.get_y()) != point.get_y() < b.get_y())
-	{
-		cross_pnt = ((b.get_x() - a.get_x()) * (point.get_y() - a.get_y()) / (b.get_y() - a.get_y())) + a.get_x();
-		if (point.get_x() < cross_pnt)
-			++cross_cnt;
-	}
+	if (!checkFunc(a, b, point, &cross_cnt))
+		return (false);
+	if (a.get_y() == b.get_y() && a.get_y() == point.get_y())
+		return (false);
 	// a-c
-	if ((point.get_y() < a.get_y()) != point.get_y() < c.get_y())
-	{
-		cross_pnt = ((c.get_x() - a.get_x()) * (point.get_y() - a.get_y()) / (c.get_y() - a.get_y())) + a.get_x();
-		if (point.get_x() < cross_pnt)
-			++cross_cnt;
-	}
+	if (!checkFunc(a, c, point, &cross_cnt))
+		return (false);
+	if (a.get_y() == c.get_y() && a.get_y() == point.get_y())
+		return (false);
 	// b-c
-	if ((point.get_y() < b.get_y()) != point.get_y() < c.get_y())
-	{
-		cross_pnt = ((c.get_x() - b.get_x()) * (point.get_y() - b.get_y()) / (c.get_y() - b.get_y())) + b.get_x();
-		if (point.get_x() < cross_pnt)
-			++cross_cnt;
-	}
+	if (!checkFunc(b, c, point, &cross_cnt))
+		return (false);
+	if (b.get_y() == c.get_y() && b.get_y() == point.get_y())
+		return (false);
 	return (cross_cnt % 2 == 1);
 }
--- cpp_module_02/ex03/main.cpp
+++ cpp_module_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tjung <tjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 05:05:51 by tjung             #+#    #+#             */
-/*   Updated: 2022/03/26 22:01:41 by tjung            ###   ########.fr       */
+/*   Updated: 2022/03/31 19:44:25 by tjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,55 @@
 int	main(void)
 {
 	Point	a_point(0, 5);
-	Point	o_point(0, 0);
-	Point	b_point(5, 0);
-	Point	internal(1, 1);
-	Point	external(5, 5);
+	Point	b_point(0, 0);
+	Point	c_point(5, 0);
+	Point	internal1(1, 1);
+	Point	internal2(2, 2);
+	Point	external1(0, 0);
+	Point	external2(1, 0);
+	Point	external3(0, 1);
+	Point	external4(5, 5);
 
 	// 삼각형 aob
-	std::cout<<"<triangle aob>"<<std::endl;
+	std::cout<<"<triangle abc>"<<std::endl;
 	std::cout<<"a_point("<<a_point.get_x()<<", "<<a_point.get_y()<<")"<<std::endl;
-	std::cout<<"o_point("<<o_point.get_x()<<", "<<o_point.get_y()<<")"<<std::endl;
 	std::cout<<"b_point("<<b_point.get_x()<<", "<<b_point.get_y()<<")"<<std::endl;
+	std::cout<<"c_point("<<c_point.get_x()<<", "<<c_point.get_y()<<")"<<std::endl;
 	std::cout<<std::endl;
 	// 삼각형 aob 의 내부, 외부 판별
 	std::cout<<"<Determine whether point is an internal>"<<std::endl;
-	std::cout<<"internal("<<internal.get_x()<<", "<<internal.get_y()<<")";
-	if (bsp(a_point, o_point, b_point, internal))
+	std::cout<<"internal1("<<internal1.get_x()<<", "<<internal1.get_y()<<")";
+	if (bsp(a_point, b_point, c_point, internal1))
 		std::cout<<" located inside"<<std::endl;
 	else
 		std::cout<<" located outside"<<std::endl;
-	std::cout<<"<Determine whether point is an external>"<<std::endl;
-	std::cout<<"external("<<external.get_x()<<", "<<external.get_y()<<")";
-	if (bsp(a_point, o_point, b_point, external))
+	std::cout<<std::endl<<"<Determine whether point is an internal>"<<std::endl;
+	std::cout<<"internal2("<<internal2.get_x()<<", "<<internal2.get_y()<<")";
+	if (bsp(a_point, b_point, c_point, internal2))
+		std::cout<<" located inside"<<std::endl;
+	else
+		std::cout<<" located outside"<<std::endl;
+	std::cout<<std::endl<<"<Determine whether point is an external>"<<std::endl;
+	std::cout<<"external1("<<external1.get_x()<<", "<<external1.get_y()<<")";
+	if (bsp(a_point, b_point, c_point, external1))
+		std::cout<<" located inside"<<std::endl;
+	else
+		std::cout<<" located outside"<<std::endl;
+	std::cout<<std::endl<<"<Determine whether point is an external>"<<std::endl;
+	std::cout<<"external2("<<external2.get_x()<<", "<<external2.get_y()<<")";
+	if (bsp(a_point, b_point, c_point, external2))
+		std::cout<<" located inside"<<std::endl;
+	else
+		std::cout<<" located outside"<<std::endl;
+	std::cout<<std::endl<<"<Determine whether point is an external>"<<std::endl;
+	std::cout<<"external3("<<external3.get_x()<<", "<<external3.get_y()<<")";
+	if (bsp(a_point, b_point, c_point, external3))
+		std::cout<<" located inside"<<std::endl;
+	else
+		std::cout<<" located outside"<<std::endl;
+	std::cout<<std::endl<<"<Determine whether point is an external>"<<std::endl;
+	std::cout<<"external4("<<external4.get_x()<<", "<<external4.get_y()<<")";
+	if (bsp(a_point, b_point, c_point, external4))
 		std::cout<<" located inside"<<std::endl;
 	else
 		std::cout<<" located outside"<<std::endl;
